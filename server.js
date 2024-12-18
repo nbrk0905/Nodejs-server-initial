c2387bde236fa5023f4e270b988e016aceaf1415
Nishant Barik <163334686+nbrk0905@users.noreply.github.com>
2024-12-18 13:10:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// Helper function to serve files
+const PORT = process.env.PORT || 3000;
+
 function serveFile(res, filePath, contentType) {
   fs.readFile(filePath, (err, content) => {
     if (err) {
@@ -15,25 +16,22 @@ function serveFile(res, filePath, contentType) {
   });
 }
 
-// Create server
 const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  if (url === '/') {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Welcome to my Server');
-  } else if (url === '/text') {
+  } else if (req.url === '/text') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('This is a text response from the server.');
-  } else if (url === '/html') {
+  } else if (req.url === '/html') {
     serveFile(res, path.join(__dirname, 'index.html'), 'text/html');
-  } else if (url === '/image') {
+  } else if (req.url === '/image') {
     serveFile(res, path.join(__dirname, 'media/image.jpg'), 'image/jpeg');
-  } else if (url === '/audio') {
+  } else if (req.url === '/audio') {
     serveFile(res, path.join(__dirname, 'media/audio.mp3'), 'audio/mpeg');
-  } else if (url === '/video') {
+  } else if (req.url === '/video') {
     serveFile(res, path.join(__dirname, 'media/video.mp4'), 'video/mp4');
-  } else if (url === '/pdf') {
+  } else if (req.url === '/pdf') {
     serveFile(res, path.join(__dirname, 'docs/sample.pdf'), 'application/pdf');
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -41,16 +39,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Start server
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
